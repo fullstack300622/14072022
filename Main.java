f4a8dce69f3f7353bc850d8637a2a9725b964290
fullstack300622 <108460728+fullstack300622@users.noreply.github.com>
2022-07-14 20:45:16 +0300

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,78 @@
+public class Main {
+    public static void main(String[] args) {
+
+////        Targil 9
+//        int num = 35;
+//        int leftNum = num / 10;
+//        int rightNum = num % 10;
+//        System.out.println(rightNum*10+leftNum);
+
+////        Targil 8
+//        int num =81;
+//        int leftNum = num / 10;
+//        int rightNum = num % 10;
+//        System.out.println(leftNum);
+//        System.out.println(rightNum);
+//        System.out.println(leftNum+rightNum);
+
+//        int num = 852;
+//        System.out.println(num/100);
+
+
+////        Targil 6
+//        int num = 4561;
+//        num %=100;
+//        num /=10;
+//        System.out.println(num);
+
+////        Targil 5
+//        int num = 12353282;
+//        System.out.println(num % 10);
+
+////        Targil 2
+//        int numOne = 4;
+//        int numTwo = 3;
+//        int sumS = numTwo * numOne;
+//        System.out.println("S: " + sumS);
+//        System.out.println("P: " + (numTwo * 2 + numOne * 2));
+
+//        int num = 12345;
+//        System.out.println(num % 1000);
+//        System.out.println(num / 1000);
+
+////        targil:4
+//        int movieLength = 134;
+//        int hours = movieLength / 60;
+//        int minutes = movieLength - hours * 60;
+//        System.out.println("hours: " + hours);
+//        System.out.println("minutes: " + minutes);
+
+////        targil:4
+//        int movieLength = 134;
+//        int hours = movieLength / 60;
+//        int minutes = movieLength % 60;
+//        System.out.println("hours: " + hours);
+//        System.out.println("minutes: " + minutes);
+
+
+//        //        Targil:1
+//        float x = 13.2f;
+//        float y = 1.2f;
+//        float z = 2.6f;
+//        float result = x + y + z;
+//        System.out.print("The result is: " + result);
+//        result = result / 3;
+//        System.out.println("Avg is: " + result);
+
+
+//        int numOne = 123;
+//        int numTwo = 516;
+//        int numThree = 51;
+////        int result = x % 10;
+////         x = x % 10;
+//        x = x /100;
+//        System.out.println(x);
+
+
+    }
+}
